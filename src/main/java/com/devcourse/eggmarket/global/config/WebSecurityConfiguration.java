f51a195c0838d0632180347df2245ab623ddd956
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-06-24 01:41:50 +0900

    🔧 config : Spring Security 관련 설정

---
+++ src/main/java/com/devcourse/eggmarket/global/config/WebSecurityConfiguration.java
@@ -0,0 +1,80 @@
+package com.devcourse.eggmarket.global.config;
+
+import com.devcourse.eggmarket.domain.user.service.UserService;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    private final UserService userService;
+
+    public WebSecurityConfiguration(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/h2-console/**"));
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, e) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("ACCESS DENIED");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().permitAll()
+            .and()
+            .csrf().disable()
+            .formLogin()
+            .defaultSuccessUrl("/")
+            .permitAll()
+            .and()
+            .httpBasic()
+            .and()
+            .authenticationProvider(authProvider())
+            .exceptionHandling()
+            .accessDeniedHandler(accessDeniedHandler())
+        ;
+
+        return http.build();
+    }
+}
