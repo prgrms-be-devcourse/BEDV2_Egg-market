476acba2494e01ef1426af63740a2e6dd15d0e17
ynoolee <heerarea129@gmail.com>
2022-06-26 21:50:22 +0900

    ✨ feat: PostAttentionRepository 추가
    - EM-32

---
+++ src/main/java/com/devcourse/eggmarket/domain/post/repository/PostAttentionRepository.java
@@ -0,0 +1,8 @@
+package com.devcourse.eggmarket.domain.post.repository;
+
+import com.devcourse.eggmarket.domain.post.model.PostAttention;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostAttentionRepository extends JpaRepository<PostAttention, Long> {
+
+}
