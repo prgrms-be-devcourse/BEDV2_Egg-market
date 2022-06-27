7f1e56a2fc258ca1cb20f428439953b78fcf96d7
ynoolee <heerarea129@gmail.com>
2022-06-27 10:28:27 +0900

    ♻️ refactor : Post 와 buyer 연관관계를 Lazy 로 변경

--- src/main/java/com/devcourse/eggmarket/domain/post/model/Post.java
+++ src/main/java/com/devcourse/eggmarket/domain/post/model/Post.java
@@ -11,6 +11,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -52,7 +53,7 @@ public class Post extends BaseEntity {
     @JoinColumn(name = "seller_id")
     private User seller;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "buyer_id")
     private User buyer;
 
