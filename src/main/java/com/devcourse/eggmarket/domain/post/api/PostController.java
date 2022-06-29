8eed985de647b6ab455ca24b3899fe40ee874710
ynoolee <heerarea129@gmail.com>
2022-06-29 15:39:11 +0900

    ♻️ refactor : Post 컨텐트 update api 변경
    - EM-29

--- src/main/java/com/devcourse/eggmarket/domain/post/api/PostController.java
+++ src/main/java/com/devcourse/eggmarket/domain/post/api/PostController.java
@@ -52,8 +52,8 @@ public class PostController {
             .body(postId);
     }
 
-    @PatchMapping("/{id}/post")
-    ResponseEntity<Long> updatePost(@RequestBody @Valid PostRequest.UpdatePost request,
+    @PatchMapping("/{id}")
+    ResponseEntity<Long> updatePost(@RequestBody PostRequest.UpdatePost request,
         Authentication authentication,
         @PathVariable Long id) {
         Long postId = postService.updatePost(id, request, authentication.getName());
