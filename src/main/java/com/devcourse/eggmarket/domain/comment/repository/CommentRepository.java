84351b9524bc7221e855498fa56cbcfcd7baf3e0
ynoolee <heerarea129@gmail.com>
2022-07-03 02:10:24 +0900

    ✨ feat : CommentRepository 에 판매글에 대하여 커서 기반 페이징을 사용하여 댓글 목록 불러오는 쿼리메소드 추가 및 테스트
    - EM-73

--- src/main/java/com/devcourse/eggmarket/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/devcourse/eggmarket/domain/comment/repository/CommentRepository.java
@@ -4,12 +4,20 @@ import com.devcourse.eggmarket.domain.comment.model.Comment;
 import com.devcourse.eggmarket.domain.post.model.Post;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByPost(Post post);
+
+    List<Comment> findAllByPost(Post post, Pageable pageable);
+
+    List<Comment> findAllByPostAndIdGreaterThan(Post post,
+        @RequestParam("id") Long lastId,
+        Pageable pageable);
 
     @Query("select c from Comment c join fetch c.post WHERE c.id = :commentId AND c.post.id = :postId")
     Optional<Comment> findByIdAndPostId(Long commentId, Long postId);
